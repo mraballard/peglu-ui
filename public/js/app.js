0d93f73129d382c39f8131caa42c24f0c7ad3bb8
Aaron Ballard <mraballard@gmail.com>
2017-01-16 13:44:25 -0300

    Changed newsletter sign up form to inline, button touching input

--- public/js/app.js
+++ public/js/app.js
@@ -6,14 +6,14 @@
   mainController.$inject = ['$scope', '$http', '$state', '$cart'];
 
   function mainController($scope, $http, $state, $cart) {
+    console.log("this:",this);
+    console.log("scope:",$scope);
     var rootUrl = 'http://localhost:4000/';
     var self = this;
 
 
 // STYLES / BOOTSTRAP
   $scope.isNavCollapsed = true;
-  $scope.isCollapsed = false;
-  $scope.isCollapsedHorizontal = false;
   }
 
 })();
